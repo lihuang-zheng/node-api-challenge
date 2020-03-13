38a786d122d3010f37987c57816d4606792f56ef
lihuang-zheng <jerryzheng0131@gmail.com>
2020-03-13 10:51:00 -0500

    fixed the file location issue.

--- routers/routerActions.js
+++ routers/routerActions.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const db = require("./data/helpers/actionModel");
+const db = require("../data/helpers/actionModel");
 const router = express.Router();
 
 router.get("/", (req, res) => {
